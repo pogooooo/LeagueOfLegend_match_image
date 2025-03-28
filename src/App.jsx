8413a4eca0dc12dd6b6b16b1842665f4608d8fde
hyunjung <poggo201103@gmail.com>
2025-03-28 21:55:55 +0900

    leader Racing image add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LeaderRacing from "./components/LeaderRacing.jsx";
+import Home from "./components/Home.jsx";
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <BrowserRouter basename="/LeagueOfLegend_match_image">
         <Routes>
-            <Route path="/" element={<App/>} />
+            <Route path="/" element={<Home/>} />
             <Route path="/leaderRacing/:roster/:position/:colors/:winner/:index/:constructor"  element={<LeaderRacing/>}></Route>
         </Routes>
     </BrowserRouter>
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+        <div>
+            this is Home and pushed at 9:55
+        </div>
+    )
+}
+
+export default Home
